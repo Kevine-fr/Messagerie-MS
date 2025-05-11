52ae1cf588a61aadfec842b20540dba54d921cff
Kevine-fr <frayfrayien@gmail.com>
2025-05-11 13:32:22 +0200

    Suppression des fichiers sauf user_service du cache Git

--- user_service/config/kafka.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-return [
-    'brokers' => env('KAFKA_BROKERS', 'kafka_messagerie:9092'), // Remplace avec ton serveur Kafka
-    'group_id' => env('KAFKA_GROUP_ID', 'my-group-id'),
-    'auto_offset_reset' => 'earliest', // Option pour consommer depuis le début des messages
-    // autres configurations...
-];
-
