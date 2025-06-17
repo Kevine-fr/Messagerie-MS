a0a91c8964a78172277f39f74979567b61fb02ed
Kevine-fr <124605694+Kevine-fr@users.noreply.github.com>
2025-06-17 16:03:23 +0200

    Ajout de getRoomName dans createMessage

--- message_service/controllers/messageController.js
+++ message_service/controllers/messageController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const socket = require('../config/socket'); 
 const Message = require('../models/Message');
 const { sendToKafka } = require('../services/kafkaProducer');
-const { getIO, getUserSocketId, isUserInConversationWith } = require('../config/socket');
+const { getIO, getUserSocketId, getRoomName } = require('../config/socket');
 
 exports.createMessage = async (req, res) => {
   try {
