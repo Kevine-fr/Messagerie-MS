b1e8cf2fabf0972e9e44a383993ebf6e7ae18bc4
Kevine-fr <frayfrayien@gmail.com>
2026-03-19 04:37:14 +0100

    feat: comment mongoDB connection on kafkaConsumer.js

--- message_service/services/kafkaConsumer.js
+++ message_service/services/kafkaConsumer.js
@@ -31,18 +31,18 @@ const topicHandlers = {
 
 const run = async () => {
   // Connexion à la base de données MongoDB
-await connectDB()
-    .then(() => {
-      console.log('🟢 Connecté à MongoDB');
-    })
-    .catch((err) => {
-      console.error('❌ Erreur de connexion à MongoDB:', err);
-    });
+// await connectDB()
+//     .then(() => {
+//       console.log('🟢 Connecté à MongoDB');
+//     })
+//     .catch((err) => {
+//       console.error('❌ Erreur de connexion à MongoDB:', err);
+//     });
 
   await consumer.connect();
 
   for (const topic of Object.keys(topicHandlers)) {
-    await consumer.subscribe({ topic, fromBeginning: true });
+    await consumer.subscribe({ topic, fromBeginning: true, allowAutoTopicCreation: true });
     console.log(`🟢 Abonné au topic "${topic}"`);
   }
 
